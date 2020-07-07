0fa2caefcf71a9f630feb15b15ba9b9083c534dc
Chenshasan <48911319+Chenshasan@users.noreply.github.com>
2020-07-07 17:46:11 +0800

    feat: 每个人平均分

--- 提交次数&分数差/answer_num.py
+++ 提交次数&分数差/answer_num.py
@@ -6,6 +6,6 @@ for key in data:
     user = data[key]
     res[key] = len(data[key])
 
-filename = 'answer_num.json'
+filename = 'answer_score.json'
 with open(filename, 'w', encoding='utf-8') as file_obj:
     json.dump(res, file_obj, ensure_ascii=False, indent=4)
